eec073a2d960460837929dce468742986b01b0c9
Julian Thomassie <julian@spatialgenomics.com>
2020-07-13 17:15:19 -0700

    Replaced print() and quit() with raise RuntimeError()

--- setup.py
+++ setup.py
@@ -14,39 +14,33 @@ URL = "https://github.com/spatial-genomics/PyVCAM"
 PACKAGES = find_packages()
 
 # Check if machine OS and architecture
-print("Checking machine attributes...")
 is_windows = platform.system().lower().find("win") > -1
 is_arch_x86_64 = platform.machine().lower().find("amd64") > -1
 is_32bit = platform.machine().lower().find("x86") > -1
 if not is_windows:
-    print("\nERROR: This package only supports Windows machines!")
-    quit()
+    raise RuntimeError("This package only supports Windows machines!")
 if not (is_32bit or is_arch_x86_64):
-    print(
-        "\nERROR: This machine architecture is not supported, it must be "
+    raise RuntimeError(
+        "This machine architecture is not supported, it must be "
         "amd64 or x86 32bit."
     )
-    quit()
 
 # Locate PVCAM install
 try:
     pvcam_sdk_path = os.environ["PVCAM_SDK_PATH"]
 except KeyError:
-    print(
-        "\nERROR: PVCAM_SDK_PATH environment variable is undefined! Please "
-        "install PVCAM SDK first!"
+    raise RuntimeError(
+        "PVCAM_SDK_PATH environment variable is undefined! "
+        "Please install PVCAM SDK first!"
     )
-    quit()
 
 # Locate correct library versions
 if is_arch_x86_64:
-    print("Machine is amd64")
     lib_dirs = [
         f"{pvcam_sdk_path}/Lib/amd64",
     ]
     libs = ["pvcam64"]
 elif is_32bit:
-    print("Machine is x86 32bit")
     lib_dirs = [
         f"{pvcam_sdk_path}/Lib/i386",
     ]
@@ -58,11 +52,7 @@ try:
 
     numpy_path = numpy.get_include()
 except ImportError:
-    print(
-        "\nERROR: numpy needs to be installed first. You can install it "
-        "via:\n\n    $ pip install numpy"
-    )
-    quit()
+    raise RuntimeError("numpy must be installed to build the pyvcam extension.")
 
 include_dirs = [
     f"{pvcam_sdk_path}/inc/",
@@ -73,7 +63,6 @@ cpp_paths = glob("pyvcam/backend/*.cpp")
 cpp_paths.append("pyvcam/pvcmodule.cpp")
 
 # Define extension
-print("\nBuilding pyvcam C++ extension...")
 pyvcam_extension = Extension(
     "pyvcam.pvc",
     cpp_paths,
