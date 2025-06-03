30dc208152d44bc820c4d6c216e57d02b915397f
Tomáš Hanák <tomas.hanak@teledyne.com>
2025-06-03 19:51:16 +0200

    Improved setup.py to support 'python -m build'
    Cleaned it to not print anything

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ from setuptools.command.build_ext import build_ext as _build_ext
 is_windows = 'win' in platform.system().lower()
 is_linux = 'lin' in platform.system().lower()
 
-print('Operating system: ' + platform.system())
-print('Machine architecture: ' + platform.machine())
+#print('Operating system: ' + platform.system())
+#print('Machine architecture: ' + platform.machine())
 
 if not is_linux and not is_windows:
-    print(' Operating systems other than Windows or Linux are not supported.')
+    print('Operating systems other than Windows or Linux are not supported.')
     quit()
 
 elif is_linux:
@@ -19,7 +19,7 @@ elif is_linux:
     is_arch_i686 = 'i686' in platform.machine().lower()
 
     if not is_arch_aarch64 and not is_arch_x86_64 and not is_arch_i686:
-        print(' Machine architecture is not supported, it must be aarch64, x86_64 or i686.')
+        print('Machine architecture is not supported, it must be aarch64, x86_64 or i686.')
         quit()
 
 elif is_windows:
@@ -27,28 +27,15 @@ elif is_windows:
     is_32bit = 'x86' in platform.machine().lower()
 
     if not is_arch_x86_64 and not is_32bit:
-        print(' Machine architecture is not supported, it must be amd64 or x86 32bit.')
+        print('Machine architecture is not supported, it must be amd64 or x86 32bit.')
         quit()
-
-if is_linux:
-    print('************************************************************\n')
-    print('Pre-install necessary packages\n')
-    print('   sudo apt install python3-pip python3-dev\n')
-    print('************************************************************\n')
-    print('Install package:   pip install .\n')
-    print('Uninstall package: pip uninstall pyvcam\n')
-    print('************************************************************\n')
-elif is_windows:
-    print('************************************************************\n')
-    print('Pre-install necessary packages as admin\n')
-    print('   python -m pip install --upgrade pip\n')
-    print('************************************************************\n')
-    print('Install package:   pip install .\n')
-    print('Uninstall package: pip uninstall pyvcam\n')
-    print('************************************************************\n')
 
 pvcam_sdk_path = os.environ['PVCAM_SDK_PATH']
 include_dirs = []
+lib_dirs = []
+libs = []
+extra_compile_args = []
+sources = []
 
 class build_ext(_build_ext):
     def finalize_options(self) -> None:
@@ -66,31 +53,30 @@ class build_ext(_build_ext):
         include_dirs.append(numpy.get_include())
 
 if is_linux:
-    extra_compile_args = ['-std=c++11']
-    include_dirs.append('{}/include/'.format(pvcam_sdk_path))
-
+    extra_compile_args.append('-std=c++11')
+    include_dirs.append('{}/include'.format(pvcam_sdk_path))
     if is_arch_aarch64:
-        lib_dirs = ['{}/library/aarch64'.format(pvcam_sdk_path)]
+        lib_dirs.append('{}/library/aarch64'.format(pvcam_sdk_path))
     elif is_arch_x86_64:
-        lib_dirs = ['{}/library/x86_64'.format(pvcam_sdk_path)]
+        lib_dirs.append('{}/library/x86_64'.format(pvcam_sdk_path))
     elif is_arch_i686:
-        lib_dirs = ['{}/library/i686'.format(pvcam_sdk_path)]
-
-    libs = ['pvcam']
+        lib_dirs.append('{}/library/i686'.format(pvcam_sdk_path))
+    libs.append('pvcam')
 
 elif is_windows:
-    extra_compile_args = []
-    include_dirs.append('{}/inc/'.format(pvcam_sdk_path))
-
+    include_dirs.append('{}/inc'.format(pvcam_sdk_path))
     if is_arch_x86_64:
-        lib_dirs = ['{}/Lib/amd64'.format(pvcam_sdk_path)]
-        libs = ['pvcam64']
+        lib_dirs.append('{}/Lib/amd64'.format(pvcam_sdk_path))
+        libs.append('pvcam64')
     elif is_32bit:
-        lib_dirs = ['{}/Lib/i386'.format(pvcam_sdk_path)]
-        libs = ['pvcam32']
+        lib_dirs.append('{}/Lib/i386'.format(pvcam_sdk_path))
+        libs.append('pvcam32')
+
+include_dirs.append('src/pyvcam')
+sources.append('src/pyvcam/pvcmodule.cpp')
 
 ext_modules = [Extension('pyvcam.pvc',
-                         ['src/pyvcam/pvcmodule.cpp'],
+                         sources=sources,
                          extra_compile_args=extra_compile_args,
                          include_dirs=include_dirs,
                          library_dirs=lib_dirs,
@@ -109,5 +95,3 @@ setup(name='pyvcam',
       setup_requires=['numpy'],
       install_requires=['numpy'],
       ext_modules=ext_modules)
-
-print('\n\n*************** Finished ***************\n')
