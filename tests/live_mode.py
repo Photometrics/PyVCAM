d2462fd3d8c909c7e09f1e59889e2e627c9abb88
Tomáš Hanák <tomas.hanak@teledyne.com>
2025-06-09 19:28:00 +0200

    Fixed all pylint and flake8 findings in live_mode.py

--- tests/live_mode.py
+++ tests/live_mode.py
@@ -1,48 +1,54 @@
 import time
+
 import cv2
 import numpy as np
 
 from pyvcam import pvc
 from pyvcam.camera import Camera
 
+
 def main():
     pvc.init_pvcam()
     cam = next(Camera.detect_camera())
     cam.open()
+    print(f'Camera: {cam.name}')
+
     cam.start_live(exp_time=20)
-    cnt = 0
-    tot = 0
-    t1 = time.time()
-    start = time.time()
+
     width = 800
     height = int(cam.sensor_size[1] * width / cam.sensor_size[0])
     dim = (width, height)
-    fps = 0
+
+    cnt = 0
+    t_start = time.time()
 
     while True:
         frame, fps, frame_count = cam.poll_frame()
-        frame['pixel_data'] = cv2.resize(frame['pixel_data'], dim, interpolation = cv2.INTER_AREA)
+        cnt += 1
+
+        frame['pixel_data'] = cv2.resize(frame['pixel_data'], dim,
+                                         interpolation=cv2.INTER_AREA)
         cv2.imshow('Live Mode', frame['pixel_data'])
 
         low = np.amin(frame['pixel_data'])
         high = np.amax(frame['pixel_data'])
-        average = np.average(frame['pixel_data'])
+        avg = np.average(frame['pixel_data'])
 
-        if cnt == 10:
-                t1 = time.time() - t1
-                fps = 10/t1
-                t1 = time.time()
-                cnt = 0
         if cv2.waitKey(10) == 27:
             break
-        print('Min:{}\tMax:{}\tAverage:{:.0f}\tFrame Rate: {:.1f}\n'.format(low, high, average, fps))
-        cnt += 1
-        tot += 1
 
-    cam.finish()
+        print(f'Min: {low}\tMax: {high}\tAverage: {avg:.0f}'
+              f'\tFrames: {frame_count}\tFrame Rate: {fps:.1f}')
+
+    t_end = time.time()
+    fps_avg = cnt / (t_end - t_start)
+    print(f'\nTotal frames: {cnt}, Average fps: {fps_avg:.1f}')
+
+    cam.abort()
+
     cam.close()
     pvc.uninit_pvcam()
 
-    print('Total frames: {}\nAverage fps: {}\n'.format(tot, (tot/(time.time()-start))))
+
 if __name__ == "__main__":
     main()
