d144257e5167c7c9be6c35127054e37e509215af
Tomáš Hanák <tomas.hanak@teledyne.com>
2025-06-10 14:16:27 +0200

    Fixed all pylint and flake8 findings in multi_camera.py

--- tests/multi_camera.py
+++ tests/multi_camera.py
@@ -1,84 +1,86 @@
-import cv2
 import threading
+
+import cv2
 
 from pyvcam import pvc
 from pyvcam.camera import Camera
 
 NUM_FRAMES = 100
 
-class TriggerThreadRun (threading.Thread):
+
+class TriggerThreadRun(threading.Thread):
     def __init__(self, cam):
         threading.Thread.__init__(self)
         self.cam = cam
         self.output = ''
 
     def run(self):
-        cnt = 0
-
         self.cam.open()
-        self.appendOutput('Camera Opened. Name: ' + self.cam.name + ' Sensor Size: ' + str(self.cam.sensor_size))
+        self.append_output(f'Camera Opened: {self.cam.name}'
+                           f'\tSensor Size: {self.cam.sensor_size}')
 
         width = 400
         height = int(self.cam.sensor_size[1] * width / self.cam.sensor_size[0])
         dim = (width, height)
 
+        cnt = 0
+
         self.cam.start_live(exp_time=20)
 
         while cnt < NUM_FRAMES:
-
             frame, fps, frame_count = self.cam.poll_frame()
+            cnt += 1
 
-            pixelData = cv2.resize(frame['pixel_data'], dim, interpolation=cv2.INTER_AREA)
-            cv2.imshow(self.cam.name, pixelData)
+            pixel_data = cv2.resize(frame['pixel_data'], dim,
+                                    interpolation=cv2.INTER_AREA)
+            cv2.imshow(self.cam.name, pixel_data)
             cv2.waitKey(10)
 
-            if self.output != '':
-                self.output += '\n'
-
-            self.appendOutput('CamName:{}\tFrame Rate: {:.1f}\tFrame Count: {:.0f}\tReturned Count: {:.0f}'.format(self.cam.name, fps, frame_count, cnt))
-            cnt += 1
+            self.append_output(f'Camera: {self.cam.name}\tFrame Rate: {fps:5.1f}'
+                               f'  Frame Count: {frame_count:3}  Returned Count: {cnt:3}')
 
         self.cam.finish()
         self.cam.close()
 
-        self.appendOutput('Camera Closed. Name: ' + self.cam.name)
+        self.append_output(f'Camera Closed: {self.cam.name}')
 
-    def appendOutput(self, outputLine):
+    def append_output(self, output_line):
         if self.output != '':
             self.output += '\n'
+        self.output += output_line
 
-        self.output += outputLine
-
-    def getOutput(self):
+    def get_output(self):
         ret = self.output
         self.output = ''
         return ret
+
 
 def main():
     pvc.init_pvcam()
 
     camera_names = Camera.get_available_camera_names()
-    print('Available cameras: ' + str(camera_names))
+    print(f'Available cameras: {camera_names}')
 
-    threadList = []
-    for cameraName in camera_names:
-        cam = Camera.select_camera(cameraName)
+    thread_list = []
+    for camera_name in camera_names:
+        cam = Camera.select_camera(camera_name)
         thread = TriggerThreadRun(cam)
         thread.start()
-        threadList.append(thread)
+        thread_list.append(thread)
 
-    checkComplete = False
-    while not checkComplete:
-        checkComplete = True
-        for thread in threadList:
-            checkComplete &= not thread.is_alive()
+    check_complete = False
+    while not check_complete:
+        check_complete = True
+        for thread in thread_list:
+            check_complete &= not thread.is_alive()
 
-            threadOutput = thread.getOutput()
-            if threadOutput != '':
-                print(threadOutput)
+            thread_output = thread.get_output()
+            if thread_output != '':
+                print(thread_output)
 
     print('All cameras complete')
     pvc.uninit_pvcam()
+
 
 if __name__ == "__main__":
     main()
