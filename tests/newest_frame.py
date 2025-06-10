1f7ab633f3f3a531ab04c165eeda943d259dd3a3
Tomáš Hanák <tomas.hanak@teledyne.com>
2025-06-10 16:39:08 +0200

    Fixed all pylint and flake8 findings in newest_frame.py

--- tests/newest_frame.py
+++ tests/newest_frame.py
@@ -1,46 +1,52 @@
-import numpy as np
-import threading
 import time
+
+import numpy as np
 
 from pyvcam import pvc
 from pyvcam.camera import Camera
 
 NUM_FRAMES = 5
-EXPOSE_TIME_ms = 200
+EXPOSE_TIME_MS = 200
 
-def printFrameDetails(frame, fps, frame_count, desc):
+
+def print_frame_details(frame, frame_count, desc):
     low = np.amin(frame['pixel_data'])
     high = np.amax(frame['pixel_data'])
-    average = np.average(frame['pixel_data'])
-    print(desc + ': Min:{}\tMax:{}\tAverage:{:.0f}\tFrame Count: {:.0f}'.format(low, high, average, frame_count))
+    avg = np.average(frame['pixel_data'])
+    print(f'{desc}:  Min: {low}\tMax: {high}\tAverage: {avg:.0f}'
+          f'\tFrames: {frame_count}')
+
 
 def main():
     # Initialize PVCAM and find the first available camera.
     pvc.init_pvcam()
-
-    cam = [cam for cam in Camera.detect_camera()][0]
+    cam = next(Camera.detect_camera())
     cam.open()
+    print(f'Camera: {cam.name}')
 
-    cam.start_seq(exp_time=EXPOSE_TIME_ms, num_frames=NUM_FRAMES)
+    cam.start_seq(exp_time=EXPOSE_TIME_MS, num_frames=NUM_FRAMES)
 
-    # Wait for 3 frames to be captured. Frames take a slightly longer than expose time, so wait a little extra time
-    time.sleep(3 * EXPOSE_TIME_ms * 1e-3 + 0.1)
+    # Wait for 3 frames to be captured.
+    # Frames take a slightly longer than expose time, so wait a little longer.
+    time.sleep(3 * EXPOSE_TIME_MS * 1e-3 + 0.1)
 
     cnt = 0
     while cnt < NUM_FRAMES:
-        frame, fps, frame_count = cam.poll_frame(oldestFrame=False)
-        printFrameDetails(frame, fps, frame_count, 'newest')
+        frame, _, frame_count = cam.poll_frame(oldestFrame=False)
+        print_frame_details(frame, frame_count, 'Newest')
 
-        frame, fps, frame_count = cam.poll_frame()
-        printFrameDetails(frame, fps, frame_count, 'oldest')
+        frame, _, frame_count = cam.poll_frame(oldestFrame=True)
+        print_frame_details(frame, frame_count, 'Oldest')
 
         cnt += 1
 
     cam.finish()
+
     cam.close()
     pvc.uninit_pvcam()
 
-    print('\nTotal frames: {}'.format(cnt))
+    print(f'\nTotal frames: {cnt}')
+
 
 if __name__ == "__main__":
     main()
