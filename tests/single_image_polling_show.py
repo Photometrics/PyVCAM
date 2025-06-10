8a75c659351616427e025cf18eaee1e504ac43c0
Tomáš Hanák <tomas.hanak@teledyne.com>
2025-06-10 16:53:08 +0200

    Fixed all pylint and flake8 findings in single_image_polling_show.py

--- tests/single_image_polling_show.py
+++ tests/single_image_polling_show.py
@@ -1,17 +1,23 @@
+from matplotlib import pyplot as plt
+
 from pyvcam import pvc
 from pyvcam.camera import Camera
-from matplotlib import pyplot as plt
+
 
 def main():
     pvc.init_pvcam()
     cam = next(Camera.detect_camera())
     cam.open()
+    print(f'Camera: {cam.name}')
 
-    frame = cam.get_frame(exp_time=20).reshape(cam.sensor_size[::-1])
-    plt.imshow(frame, cmap="gray")
+    frame = cam.get_frame(exp_time=20)
+
+    plt.imshow(frame.reshape(cam.sensor_size[::-1]), cmap='gray')
     plt.show()
+
     cam.close()
     pvc.uninit_pvcam()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
