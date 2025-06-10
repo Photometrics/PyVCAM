99e8bb95c46c566c8462d1915444525e2c4582ff
Tomáš Hanák <tomas.hanak@teledyne.com>
2025-06-10 16:52:47 +0200

    Fixed all pylint and flake8 findings in single_image_polling.py

--- tests/single_image_polling.py
+++ tests/single_image_polling.py
@@ -3,16 +3,18 @@ from pyvcam.camera import Camera
 
 
 def main():
-    # Initialize PVCAM and find the first available camera.
     pvc.init_pvcam()
-    cam = [cam for cam in Camera.detect_camera()][0]
+    cam = next(Camera.detect_camera())
     cam.open()
-    cam.speed_table_index = 0
-    for i in range(5):
+    print(f'Camera: {cam.name}')
+
+    for _ in range(5):
         frame = cam.get_frame(exp_time=20)
-        print("First five pixels of frame: {}, {}, {}, {}, {}".format(*frame[:5]))
+        print(f"First five pixels: {frame[0, 0:5]}")
+
     cam.close()
     pvc.uninit_pvcam()
+
 
 if __name__ == "__main__":
     main()
