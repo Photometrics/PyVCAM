470f11909f7abb40920a475d8885e57ec4f5fc59
Tomáš Hanák <tomas.hanak@teledyne.com>
2025-06-19 17:17:03 +0200

    Extended seq_mode.py example
    - It demonstrates that the list of VTM exposures can be shorted than number of frames to acquire.
    - Added code that shows it is not possible to call poll_frame outside active acquisition.

--- tests/seq_mode.py
+++ tests/seq_mode.py
@@ -8,6 +8,14 @@ from pyvcam import constants as const
 NUM_FRAMES = 10
 
 
+def print_frame_details(frame, frame_count, extra_info=''):
+    low = np.amin(frame)
+    high = np.amax(frame)
+    avg = np.average(frame)
+    print(f'Min: {low}\tMax: {high}\tAverage: {avg:.0f}'
+          f'\tFrames: {frame_count}{extra_info}')
+
+
 def main():
     pvc.init_pvcam()
     cam = next(Camera.detect_camera())
@@ -16,16 +24,13 @@ def main():
 
     cnt = 0
 
-    print('\nUsing start_seq+poll_frame:')
+    print('\nUsing start_seq+poll_frame+finish:')
     cam.start_seq(exp_time=20, num_frames=NUM_FRAMES)
     while cnt < NUM_FRAMES:
         frame, fps, frame_count = cam.poll_frame()
         cnt += 1
-        low = np.amin(frame['pixel_data'])
-        high = np.amax(frame['pixel_data'])
-        avg = np.average(frame['pixel_data'])
-        print(f'Min: {low}\tMax: {high}\tAverage: {avg:.0f}\tFrames: {frame_count}'
-              f'\tFrame Rate: {fps:.1f}')
+        print_frame_details(frame['pixel_data'], frame_count,
+                            f'\tFrame Rate: {fps:.1f}')
         time.sleep(0.05)
     cam.finish()
 
@@ -33,23 +38,30 @@ def main():
     frames = cam.get_sequence(NUM_FRAMES)
     for frame in frames:
         cnt += 1
-        low = np.amin(frame)
-        high = np.amax(frame)
-        avg = np.average(frame)
-        print(f'Min: {low}\tMax: {high}\tAverage: {avg:.0f}\tFrames: {cnt}')
+        print_frame_details(frame, cnt)
 
     print('\nUsing get_vtm_sequence:')
-    time_list = [i * 10 for i in range(1, NUM_FRAMES + 1)]
+    time_list = [i * 10 for i in range(1, 8)]
     frames = cam.get_vtm_sequence(time_list, const.EXP_RES_ONE_MILLISEC, NUM_FRAMES)
     i = 0
     for frame in frames:
         cnt += 1
-        low = np.amin(frame)
-        high = np.amax(frame)
-        avg = np.average(frame)
-        print(f'Min: {low}\tMax: {high}\tAverage: {avg:.0f}\tFrames: {cnt}'
-              f'\tExp. time: {time_list[i]}')
-        i += 1
+        print_frame_details(frame, cnt, f'\tExp. time: {time_list[i]}')
+        i = (i + 1) % len(time_list)
+
+    try:
+        print('\nUsing poll_frame without starting acquisition:')
+        frame, fps, frame_count = cam.poll_frame()
+        cnt += 1
+        print_frame_details(frame['pixel_data'], frame_count,
+                            f'\tFrame Rate: {fps:.1f}')
+    except RuntimeError as e:
+        print(f'Expected error: {e}')
+
+    print('\nUsing get_frame alone:')
+    frame = cam.get_frame()
+    cnt += 1
+    print_frame_details(frame, cnt)
 
     cam.close()
     pvc.uninit_pvcam()
